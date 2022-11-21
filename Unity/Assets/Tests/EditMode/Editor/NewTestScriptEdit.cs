a67a2cc4eaf7c3d788093e5d500c6476828d8958
iori tobeta <15eb1137@gmail.com>
2022-11-21 09:33:46 +0900

    Edit mode sample test

--- Unity/Assets/Tests/EditMode/Editor/NewTestScriptEdit.cs
+++ Unity/Assets/Tests/EditMode/Editor/NewTestScriptEdit.cs
@@ -1,25 +1,20 @@
 using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
-using UnityEngine;
 using UnityEngine.TestTools;
+using Assert = UnityEngine.Assertions.Assert; // AssertはNUnitのではなくUnityのものを使う
 
-public class NewTestScript
+public class NewEditModeTest
 {
-    // A Test behaves as an ordinary method
     [Test]
-    public void NewTestScriptSimplePasses()
+    public void SimplePasses()
     {
-        // Use the Assert class to test conditions
+        Assert.AreEqual(2, 1 + 1);
     }
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
     [UnityTest]
-    public IEnumerator NewTestScriptWithEnumeratorPasses()
+    public IEnumerator EnumeratorPasses()
     {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
         yield return null;
+        Assert.AreEqual(2, 1 + 1);
     }
 }
